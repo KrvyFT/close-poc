beef234c4f307d4d10a2bbf21105bedcdeca0205
KrvyFT <KrvyFT@proton.me>
2024-07-07 18:07:33 +0800

    feat: Async POST request handling with custom config and error handling

--- src/handle.rs
+++ src/handle.rs
@@ -1,15 +1,35 @@
 use std::error::Error;
 
+use reqwest::header::{self, HeaderMap, USER_AGENT};
+
 use crate::poc::Poc;
 
 impl Poc {
-    pub async fn check(self, url: &str) -> Result<(), Box<dyn Error>> {
+    pub async fn req_get(&self, url: &str) -> Result<(), Box<dyn Error>> {
         let res = reqwest::Client::builder()
             .proxy(reqwest::Proxy::all("http://127.0.0.1:8080")?)
             .build()?;
-        let urll = url.to_owned() + &self.requests.payload;
-        res.get(urll).json(&self.requests).send().await?;
+        let full_url = format!("{}{}", url, self.requests.payload);
+        res.get(full_url)
+            .header(USER_AGENT, self.requests.headers.user_agent.as_str())
+            .send()
+            .await?;
 
+        Ok(())
+    }
+
+    pub async fn req_post(&self, url: &str) -> Result<(), Box<dyn Error>> {
+        let res = reqwest::Client::builder()
+            .danger_accept_invalid_certs(true)
+            .proxy(reqwest::Proxy::all("http://127.0.0.1:8080")?)
+            .build()?;
+        let full_url = format!("{}{}", url, self.requests.payload);
+        let headers = HeaderMap::new();
+        res.post(full_url)
+            .headers(self.requests.headers.to_maps())
+            .body(self.requests.data.clone())
+            .send()
+            .await?;
         Ok(())
     }
 }
@@ -18,13 +38,19 @@ impl Poc {
 mod tests {
     use std::error::Error;
 
-    use crate::poc::{self, Pocs};
+    use crate::{
+        poc::{self, Pocs},
+        urls::{self, read_from_file},
+    };
 
     #[tokio::test]
     async fn test_check_vulnerabilities() -> Result<(), Box<dyn Error>> {
-        let pocs = Pocs::from_json("src/poc.json").unwrap();
-        for poc in pocs {
-            poc.check("http://27.174.121.119:8001").await?;
+        let pocs = Pocs::from_json("testdata/post.json").unwrap();
+        let urls = read_from_file("testdata/ps.txt").await.unwrap();
+
+        let poc = pocs.0.get(0).unwrap();
+        for url in &urls {
+            poc.req_post(&format!("http://{}", url)).await?;
         }
 
         Ok(())
